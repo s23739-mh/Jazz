6e02fd9a9980ce5e592f725a4b1623b67214e556
s23739-mh <73026549+s23739-mh@users.noreply.github.com>
2022-03-12 13:47:32 +0100

    Ale Jazz

---
+++ mikhol/src/main/java/pj/pjatk/mikhol/MikholApplication.java
@@ -0,0 +1,13 @@
+package pj.pjatk.mikhol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikholApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikholApplication.class, args);
+	}
+
+}
