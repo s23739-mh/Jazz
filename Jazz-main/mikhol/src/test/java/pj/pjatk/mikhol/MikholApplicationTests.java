29c6aea18326d004071ba93faf147bf15616e1e8
s23739-mh <73026549+s23739-mh@users.noreply.github.com>
2022-03-26 09:05:20 +0100

    pog

---
+++ Jazz-main/mikhol/src/test/java/pj/pjatk/mikhol/MikholApplicationTests.java
@@ -0,0 +1,13 @@
+package pj.pjatk.mikhol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikholApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
